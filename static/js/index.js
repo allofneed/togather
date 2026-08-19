3af5eef9916fe19d902210938203a68a36d15c75
your_name <your_email@example.com>
2026-08-19 11:16:45 +0900

    width

--- static/js/index.js
+++ static/js/index.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     markerImgSrc = "/static/svg/map_log/marker_togetthe.svg";
                 }
                 var storeMarkerHtml = `
-                    <div style="width: 40px; height: 40px; pointer-events: none;">
+                    <div style="width: 70px; height: 70px; pointer-events: none;">
                         <img src="${markerImgSrc}" alt="${category}" style="width: 100%; height: 100%; object-fit: contain;">
                     </div>
                 `;
